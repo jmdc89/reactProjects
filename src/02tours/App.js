0929b84157badc8f9f365adcb76618a74939b210
Jose Manuel <jmdcarq89@gmail.com>
2021-02-26 19:11:25 +0100

    fetch data

--- src/02tours/App.js
+++ src/02tours/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import Loading from './Loading';
+import Tours from './Tours';
 import './index.css';
 import Tour from './Tour';
 
@@ -8,9 +8,29 @@ import Tour from './Tour';
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
+
   const [loading, setLoading] = useState(true);
   const [tours,setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  },[]);
 
   if (loading) {
     return (
@@ -21,7 +41,7 @@ function App() {
   }
   return (
     <main>
-        <Tour/>
+        <Tours tours={tours}/>
     </main>
   );
 }
