5dcc9047198dcc40a6a67089c692173e6c63bc24
Jose Manuel <jmdcarq89@gmail.com>
2021-02-26 18:19:47 +0100

    birthday reorg

--- 1-birthday-reminder/src/index.js
+++ 1-birthday-reminder/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './01birthday/index.css';
+import App from './01birthday/App';
 
 ReactDOM.render(
   <React.StrictMode>
