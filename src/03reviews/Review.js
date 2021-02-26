5998f2b2201fa04b47baf1940668ee0e01612e75
Jose Manuel <jmdcarq89@gmail.com>
2021-02-27 00:10:10 +0100

    buttons config

--- src/03reviews/Review.js
+++ src/03reviews/Review.js
@@ -5,7 +5,35 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
   const [index, setIndex] = useState(0);
-  const {name,job,image,text} = people[index];
+  const { name, job, image, text } = people[index];
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
 
   return (
       <article className='reviewr'>
@@ -25,7 +53,7 @@ const Review = () => {
               <button className='next-btn-mod' onClick={nextPerson}>
                 <FaChevronRight/>
               </button>
-              <button className='random-btn-mod'>surprise me</button>
+              <button className='random-btn-mod' onClick={randomPerson}>surprise me</button>
           </div>
       </article>
   );
