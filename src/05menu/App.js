9b462d03fd1d128cec7a22bdb1aa4644c2f1bebf
Jose Manuel <jmdcarq89@gmail.com>
2021-02-27 22:51:49 +0100

    Menu App

--- src/05menu/App.js
+++ src/05menu/App.js
@@ -2,9 +2,24 @@ import React, { useState } from 'react';
 import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
+import './menu.css';
 
-function App() {
-  return <h2>menu project setup</h2>;
+function MenuApp() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  return (
+    <main>
+      <section className="menu section-m">
+        <div className="title-m">
+          <h2>our menu</h2>
+          <div className="underline-m"></div>
+        </div>
+        <Categories categories={categories}  />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
-export default App;
+export default MenuApp;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import BirthdayApp from './01birthday/App';
 import Tours from './02tours/App';
 import Reviews from './03reviews/App';
 import Accordion from './04accordion/App';
+import MenuApp from './05menu/App';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <BirthdayApp />
-    <Tours/> */}
-    {/* <Reviews/> */}
-    <Accordion/>
+    <Tours/>
+    <Reviews/>
+    <Accordion/> */}
+    <MenuApp/>
   </React.StrictMode>,
   document.getElementById('root')
 );
