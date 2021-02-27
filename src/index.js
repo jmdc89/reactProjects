4b1cce97cfbf27f7ba5a4f3f0151be420f78048a
Jose Manuel <jmdcarq89@gmail.com>
2021-02-27 22:35:29 +0100

    folders04-06

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import BirthdayApp from './01birthday/App';
 import Tours from './02tours/App';
 import Reviews from './03reviews/App';
+import Accordion from './04accordion/App';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <BirthdayApp />
     <Tours/> */}
-    <Reviews/>
+    {/* <Reviews/> */}
+    <Accordion/>
   </React.StrictMode>,
   document.getElementById('root')
 );
