75d802939823f5abaf4ce3d9df27032e7ca66e22
Jose Manuel <jmdcarq89@gmail.com>
2021-02-28 21:49:05 +0100

    fetch and usestate

--- src/06tabs/App.js
+++ src/06tabs/App.js
@@ -5,24 +5,43 @@ import './tabs.css';
 const url = 'https://course-api.com/react-tabs-project'
 
 function Tabs() {
-  
-  
+  const [loading, setLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+  const [value, setValue] = useState(0)
 
+  const fetchJobs = async () => {
+    const reponse = await fetch(url)
+    const newJobs = await reponse.json()
+    setJobs(newJobs)
+    setLoading(false)
+  }
+  useEffect(() => {
+    fetchJobs()
+  }, [])
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    )
+  }
+  const { company, dates, duties, title } = jobs[value]
+  
   return (
-    <section className="section">
-      <div className="title">
+    <section className="section-t">
+      <div className="title-t">
         <h2>experience</h2>
-        <div className="underline"></div>
+        <div className="underline-t"></div>
       </div>
-      <div className="jobs-center">
+      <div className="jobs-center-t">
         {/* btn container */}
-        <div className="btn-container">
+        <div className="btn-container-t">
           {jobs.map((item, index) => {
             return (
               <button
                 key={item.id}
                 onClick={() => setValue(index)}
-                className={`job-btn ${index === value && 'active-btn'}`}
+                className={`job-btn-t ${index === value && 'active-btn-t'}`}
               >
                 {item.company}
               </button>
@@ -30,21 +49,21 @@ function Tabs() {
           })}
         </div>
         {/* job info */}
-        <article className="job-info">
+        <article className="job-info-t">
           <h3>{title}</h3>
           <h4>{company}</h4>
-          <p className="job-date">{dates}</p>
+          <p className="job-date-t">{dates}</p>
           {duties.map((duty, index) => {
             return (
-              <div key={index} className="job-desc">
-                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+              <div key={index} className="job-desc-t">
+                <FaAngleDoubleRight className="job-icon-t"></FaAngleDoubleRight>
                 <p>{duty}</p>
               </div>
             )
           })}
         </article>
       </div>
-      <button type="button" className="btn">
+      <button type="button" className="btn-t">
         more info
       </button>
     </section>
