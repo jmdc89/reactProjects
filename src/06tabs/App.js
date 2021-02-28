8f71e235e2198a9dbd0b2a2e8da657f016129d13
Jose Manuel <jmdcarq89@gmail.com>
2021-02-28 21:45:28 +0100

    Tabs return

--- src/06tabs/App.js
+++ src/06tabs/App.js
@@ -5,7 +5,50 @@ import './tabs.css';
 const url = 'https://course-api.com/react-tabs-project'
 
 function Tabs() {
-  return <h2>tabs project setup</h2>
+  
+  
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        {/* btn container */}
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && 'active-btn'}`}
+              >
+                {item.company}
+              </button>
+            )
+          })}
+        </div>
+        {/* job info */}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+      <button type="button" className="btn">
+        more info
+      </button>
+    </section>
+  )
 }
 
 export default Tabs
