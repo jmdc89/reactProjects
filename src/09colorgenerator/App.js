8827453d8689e04f8c4280d3df318fdc7417e0f5
Jose Manuel <jmdcarq89@gmail.com>
2021-03-02 22:09:40 +0100

    return

--- src/09colorgenerator/App.js
+++ src/09colorgenerator/App.js
@@ -5,17 +5,46 @@ import './colorgenerator.css';
 import Values from 'values.js'
 
 function ColorGenerator() {
-  
+
   const [color,setColor] = useState('');
   const [error,setError] = useState(false);
   const [list,setList] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
   }
 
-  return <h2>color generator project</h2>
+  return (
+    <>
+      <section className='container-cg'>
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder='#f15025'
+            className={`${error ? 'error-cg' : null}`}
+          />
+          <button className='btn-cg' type='submit'>
+            submit
+          </button>
+        </form>
+      </section>
+      <section className='colors-cg'>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          )
+        })}
+      </section>
+    </>
+  )
 }
 
 export default ColorGenerator;
