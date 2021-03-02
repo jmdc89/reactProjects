e1e5b3d7d2d1b04e0d1ec707d60f86b43f7e9056
Jose Manuel <jmdcarq89@gmail.com>
2021-03-02 21:58:32 +0100

    const and handlesubmit

--- src/09colorgenerator/App.js
+++ src/09colorgenerator/App.js
@@ -2,9 +2,19 @@ import React, { useState } from 'react'
 import SingleColor from './SingleColor'
 import './colorgenerator.css';
 
-// import Values from 'values.js'
+import Values from 'values.js'
 
 function ColorGenerator() {
+  
+  const [color,setColor] = useState('');
+  const [error,setError] = useState(false);
+  const [list,setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  }
+
   return <h2>color generator project</h2>
 }
 
