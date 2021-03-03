f919d0a92888feeef16c86461a449e44c02f77a1
Jose Manuel <jmdcarq89@gmail.com>
2021-03-03 20:57:48 +0100

    cards array

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,11 +4,14 @@ import birthday from '../assets/01birthday.png';
 
 function Card() {
     return (
-        <div className="card">
-            <img src={birthday} alt=""/>
-            <div className="card-body">
+        <div className="card text-center bg-dark">
+            <img src="" alt=""/>
+            <div className="card-body text-light">
                 <h4 className="card-title">My title</h4>
                 <p className="card-text text-secondary">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                <a href="#!" className="btn btn-outline-secondary rounded-0">
+                    Go to this website
+                </a>
             </div>
         </div>
         
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,28 @@
 import React from 'react'
 import Card from './Card';
 
+import birthday from '../assets/01birthday.png';
+import tours from '../assets/02tours.png';
+import reviews from '../assets/03reviews.png';
+
+const cards = [
+    {
+        id:1,
+        title: 'Birthday',
+        image: birthday
+    },
+    {
+        id:2,
+        title: 'Tours',
+        image: tours
+    },
+    {
+        id:3,
+        title: 'Reviews',
+        image: reviews
+    },
+]
+
 function Cards() {
     return (
         <div className="container d-flex justify-content-center h-100 align-items-center">
@@ -8,13 +30,6 @@ function Cards() {
                 <div className="col-md-4">
                     <Card/>
                 </div>
-                <div className="col-md-4">
-                    <Card/>
-                </div>
-                <div className="col-md-4">
-                    <Card/>
-                </div>
-               
             </div>
             
         </div>
