3475c1ab89619c8a6a13a496f296d216aeb28c82
Jose Manuel <jmdcarq89@gmail.com>
2021-03-05 22:32:30 +0100

    return grocery

--- src/10grocerybud/App.js
+++ src/10grocerybud/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
 
+import './grocery.css';
+
 function Grocery() {
   const [name, setName] = useState('');
   const [list, setList] = useState(getLocalStorage());
@@ -9,7 +11,35 @@ function Grocery() {
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
 
-  return <h2>grocery bud setup</h2>
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+          <input
+            type='text'
+            className='grocery'
+            placeholder='e.g. eggs'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className='grocery-container'>
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button className='clear-btn' onClick={clearList}>
+            clear items
+          </button>
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default Grocery
